c72f0cf12a1c6245c7bdae1a33c1c6610413fa9c
DanT329 <dterry16@byu.edu>
2024-05-23 13:57:20 -0600

    removed redundant check.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -188,7 +188,7 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        if (isInCheck(teamColor) || isInCheckmate(teamColor)) {
+        if (isInCheck(teamColor)) {
             return false;
         }else{
             ArrayList<ChessPosition> teamPieces = findFriendlyPieces(teamColor);
