1c6117d69161e943391f9f136bc0fe698867567a
DanT329 <140003581+DanT329@users.noreply.github.com>
2024-05-30 13:46:47 -0600

    testing for listGames() and updateGame().

--- server/src/test/java/dataaccess/DataAccessMySQLGameTest.java
+++ server/src/test/java/dataaccess/DataAccessMySQLGameTest.java
@@ -2,13 +2,14 @@ package dataaccess;
 
 import dataaccess.mysql.DataAccessMySQLGame;
 import model.GameData;
-import model.UserData;
 
 import java.sql.SQLException;
+import java.util.Collection;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.AppService;
+import service.exception.AlreadyTakenException;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class DataAccessMySQLGameTest {
@@ -31,7 +32,63 @@ public class DataAccessMySQLGameTest {
         DataAccessMySQLGame dataAccess = new DataAccessMySQLGame();
         GameData gameInfo = new GameData(0,null,null,"Mygame",null);
         dataAccess.addGame(gameInfo);
-        assertThrows(DataAccessException.class, () -> {dataAccess.addGame(gameInfo);});
+        assertThrows(DataAccessException.class, () -> dataAccess.addGame(gameInfo));
+    }
+
+    @Test
+    public void getGameGood() throws SQLException, DataAccessException {
+        DataAccessMySQLGame dataAccess = new DataAccessMySQLGame();
+        GameData gameInfo = new GameData(0,null,null,"Mygame",null);
+        dataAccess.addGame(gameInfo);
+        GameData actualGame = dataAccess.getGame(gameInfo);
+        assertEquals(gameInfo.gameName(), actualGame.gameName());
+    }
+    @Test
+    public void getGameNull() throws SQLException, DataAccessException {
+        DataAccessMySQLGame dataAccess = new DataAccessMySQLGame();
+        GameData gameInfo = new GameData(0,null,null,"Mygame",null);
+        dataAccess.addGame(gameInfo);
+        GameData badGameInfo = new GameData(0,null,null,"BadName",null);
+        GameData actualGame = dataAccess.getGame(badGameInfo);
+        assertNull(actualGame);
+    }
+
+    @Test
+    public void updateGameGood() throws SQLException, DataAccessException, AlreadyTakenException {
+        DataAccessMySQLGame dataAccess = new DataAccessMySQLGame();
+        GameData gameInfo = new GameData(0,null,null,"Mygame",null);
+        dataAccess.addGame(gameInfo);
+        gameInfo = dataAccess.getGame(gameInfo);
+        GameData gameInfoUpdated = new GameData(gameInfo.gameID(),"Jimmy",null,"Mygame",null);
+        dataAccess.updateGame(gameInfoUpdated);
+        gameInfo = dataAccess.getGame(gameInfo);
+        assertEquals(gameInfo.whiteUsername(), "Jimmy");
+    }
+    @Test
+    public void updateGameAlreadyTaken() throws SQLException, DataAccessException, AlreadyTakenException {
+        DataAccessMySQLGame dataAccess = new DataAccessMySQLGame();
+        GameData gameInfo = new GameData(0,null,null,"Mygame",null);
+        dataAccess.addGame(gameInfo);
+        gameInfo = dataAccess.getGame(gameInfo);
+        GameData gameInfoUpdated = new GameData(gameInfo.gameID(),"Jimmy",null,"Mygame",null);
+        dataAccess.updateGame(gameInfoUpdated);
+        assertThrows(AlreadyTakenException.class, () -> dataAccess.updateGame(gameInfoUpdated));
+    }
+
+    @Test
+    public void listGamesGood() throws SQLException, DataAccessException {
+        DataAccessMySQLGame dataAccess = new DataAccessMySQLGame();
+        GameData gameInfo = new GameData(0,null,null,"Mygame",null);
+        dataAccess.addGame(gameInfo);
+        Collection<GameData> games = dataAccess.getAllGames();
+        assertNotNull(games);
+    }
+
+    @Test
+    public void listGamesEmpty() throws SQLException, DataAccessException {
+        DataAccessMySQLGame dataAccess = new DataAccessMySQLGame();
+        Collection<GameData> games = dataAccess.getAllGames();
+        assertTrue(games.isEmpty());
     }
 
 }
