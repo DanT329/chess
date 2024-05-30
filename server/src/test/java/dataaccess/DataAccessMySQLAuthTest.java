fa74aed4cae6d5d195fc5840264e62900d4fca0c
DanT329 <140003581+DanT329@users.noreply.github.com>
2024-05-30 13:56:06 -0600

    auth tests all written.

--- server/src/test/java/dataaccess/DataAccessMySQLAuthTest.java
+++ server/src/test/java/dataaccess/DataAccessMySQLAuthTest.java
@@ -2,9 +2,7 @@ package dataaccess;
 import dataaccess.mysql.DataAccessMySQLAuth;
 import model.AuthData;
 import model.UserData;
-
 import java.sql.SQLException;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.AppService;
@@ -27,4 +25,49 @@ public class DataAccessMySQLAuthTest {
         assertEquals(auth.username(),user.username());
     }
 
+    @Test
+    public void createAuthSeveral() throws SQLException,DataAccessException {
+        UserData user = new UserData("user", "password","email@email.com");
+        DataAccessMySQLAuth dataAccess = new DataAccessMySQLAuth();
+        AuthData auth = dataAccess.createAuth(user);
+        auth = dataAccess.createAuth(user);
+        assertEquals(auth.username(),user.username());
+    }
+
+    @Test
+    public void verifyAuthGood() throws SQLException,DataAccessException {
+        UserData user = new UserData("user", "password","email@email.com");
+        DataAccessMySQLAuth dataAccess = new DataAccessMySQLAuth();
+        AuthData auth = dataAccess.createAuth(user);
+        AuthData actual = dataAccess.verifyToken(auth.authToken());
+        assertEquals(auth.username(),actual.username());
+    }
+
+    @Test
+    public void verifyAuthBadToken() throws SQLException,DataAccessException {
+        UserData user = new UserData("user", "password","email@email.com");
+        DataAccessMySQLAuth dataAccess = new DataAccessMySQLAuth();
+        AuthData auth = dataAccess.createAuth(user);
+        AuthData actual = dataAccess.verifyToken("BadToken");
+        assertNull(actual);
+    }
+
+    @Test
+    public void deleteAuthTokenGood() throws SQLException,DataAccessException {
+        UserData user = new UserData("user", "password","email@email.com");
+        DataAccessMySQLAuth dataAccess = new DataAccessMySQLAuth();
+        AuthData auth = dataAccess.createAuth(user);
+        dataAccess.deleteAuth(auth);
+        assertNull(dataAccess.verifyToken(auth.authToken()));
+    }
+
+    @Test
+    public void deleteAuthTokenMultiple() throws SQLException,DataAccessException {
+        UserData user = new UserData("user", "password","email@email.com");
+        DataAccessMySQLAuth dataAccess = new DataAccessMySQLAuth();
+        AuthData auth = dataAccess.createAuth(user);
+        dataAccess.deleteAuth(auth);
+        dataAccess.deleteAuth(auth);
+        assertNull(dataAccess.verifyToken(auth.authToken()));
+    }
 }
