5c8c0aaf0bd1f5d736b4b0e7d74bea1012fb857a
DanT329 <140003581+DanT329@users.noreply.github.com>
2024-05-30 12:38:27 -0600

    Created ChessGame Deserializer. Board is nesting another board when serialized so a custom class is needed to handle this. Overwrite ChessPosition toString(). Wrote test to convert ChessGame to and from json.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -212,5 +213,18 @@ public class ChessGame {
             }
         }
         return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessGame chessGame = (ChessGame) o;
+        return teamTurn == chessGame.teamTurn && Objects.equals(board, chessGame.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamTurn, board);
     }
 }
