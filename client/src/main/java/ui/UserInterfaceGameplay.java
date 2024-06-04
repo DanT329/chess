4fd494de09ef514dfc2517ed0f5a5697de1f886c
DanT329 <140003581+DanT329@users.noreply.github.com>
2024-06-04 11:38:27 -0600

    Start phase 6

---
+++ client/src/main/java/ui/UserInterfaceGameplay.java
@@ -0,0 +1,24 @@
+package ui;
+
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import client.ServerFacade;
+import model.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class UserInterfaceGameplay {
+
+    private ChessGame chessGame;
+    private ChessBoard chessBoard;
+
+    public void run(){
+        //TODO: Add commands and interaction with websocket
+    }
+}
