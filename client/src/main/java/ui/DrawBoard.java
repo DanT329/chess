1403074c42983f6475a3bc0487aa3c0bfcde345b
DanT329 <140003581+DanT329@users.noreply.github.com>
2024-06-06 12:45:00 -0600

    highlight moves now works.

---
+++ client/src/main/java/ui/DrawBoard.java
@@ -0,0 +1,11 @@
+package ui;
+
+import chess.ChessGame;
+import chess.ChessPosition;
+
+public class DrawBoard {
+
+    public static void highLightMoves(ChessGame game, ChessPosition position){
+
+    }
+}
